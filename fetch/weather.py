eac7ed20bdbeef7072bae11cd25836d81ae9523b
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-08-31 23:19:57 +0300

    Creating Weather class

---
+++ fetch/weather.py
@@ -0,0 +1,14 @@
+from libraries.fetch_data import FetchData
+
+class Weather:
+    def __init__(self, city:str):
+        self.city = city
+
+    def fetch_weather(self):
+        url = f'https://www.ventusky.com/{self.city.lower()}'
+        print(url)
+        FetchData(url=url, file_name=f'{self.city}_weather').run()
+
+if __name__=='__main__':
+    Weather('minsk').fetch_weather()
+    Weather('erevan').fetch_weather()

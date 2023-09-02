58d70111314164e36d7910b5654aaa2f91e89dd4
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-09-02 12:40:56 +0300

    Setting up schema schema.py

---
+++ api/schema.py
@@ -0,0 +1,68 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class PetrolBase(BaseModel):
+    usd_price_per_litre: float
+    usd_price_per_gallon: float
+    eur_price_per_litre: float
+    eur_price_per_gallon: float
+class PetrolCreate(PetrolBase):
+    country: str 
+class Petrol(PetrolBase):
+    id: int
+    country: str
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class WeatherBase(BaseModel):
+    temperature: float
+    temperature_unit: str
+    wind: float
+    wind_unit: str
+    visibility: Optional[float]
+    visibility_unit: Optional[str]
+    humidity: Optional[float]
+    humidity_unit: Optional[str]
+    clouds: float
+    clouds_unit: str
+    cloud_base: float
+    cloud_base_unit: str
+class WeatherCreate(WeatherBase):
+    country: str 
+class Weather(WeatherBase):
+    id: int
+    country: str
+    created_at: datetime
+    updated_at: datetime
+    class Config:
+        orm_mode = True
+
+
+class ExchangeRateBase(BaseModel):
+    usd_rate: float
+    euro_rate: float
+class ExchangeRateCreate(ExchangeRateBase):
+    country: str 
+class ExchangeRate(ExchangeRateBase):
+    id: int
+    country: str
+    created_at: datetime
+    updated_at: datetime
+    class Config:
+        orm_mode = True
+
+
+class PopulationBase(BaseModel):
+    total_population: int
+class PopulationCreate(PopulationBase):
+    country: str
+class Population(PopulationBase):
+    created_at: datetime
+    updated_at: datetime
+    class Config:
+        orm_mode = True

23bf51b90fc50fe913416c8a248c93931d2fe70c
rupliwiec <116656530+rupliwiec@users.noreply.github.com>
2023-09-02 14:02:41 +0300

    CRUD for exchange rate

--- api/crud.py
+++ api/crud.py
@@ -78,3 +78,33 @@ def delete_weather(weather_id: str, db: Session):
     db.commit()
     return {"delete": True, "Item": weather_item}
 
+
+def create_exchange_rate(db: Session, exchange: schema.ExchangeRateCreate):
+    db_item = models.Weather(**exchange.dict())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def get_exchange_rate(db: Session, exchange_rate_id: str):
+    return db.query(models.ExchangeRate).filter(models.ExchangeRate.id == exchange_rate_id).first()
+
+def get_exchange_rates(db: Session, skip: int = 0, limit = 50):
+    return db.query(models.ExchangeRate).offset(skip).limit(limit).all()
+
+def update_exchange_rate(exchange_rate_id: str, db: Session, updated_exchange_rate: schema.ExchangeRateCreate):
+    exchange_rate_to_update = db.query(models.ExchangeRate).filter(models.ExchangeRate.id == exchange_rate_id).first()
+    exchange_rate_to_update.usd_rate = updated_exchange_rate.usd_rate
+    exchange_rate_to_update.euro_rate = updated_exchange_rate.euro_rate
+    exchange_rate_to_update.country = updated_exchange_rate.country
+    exchange_rate_to_update.updated_at = datetime.datetime.utcnow()
+    db.commit()
+    return exchange_rate_to_update
+
+def delete_exchange_rate(exchange_rate_id: str, db: Session):
+    exchange_rate_item = db.query(models.ExchangeRate).filter(models.ExchangeRate.id == exchange_rate_id).first()
+    if exchange_rate_item is None:
+        raise HTTPException(status_code=404, detail="ExchangeRate value not found")
+    db.delete(exchange_rate_item)
+    db.commit()
+    return {"delete": True, "Item": exchange_rate_item}
